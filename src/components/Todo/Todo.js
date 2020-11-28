a69b7fc443bae3cdf4f1913a4c2c1d35ba290c62
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2020-11-28 02:35:27 -0800

    first commit

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import TodoForm from "../Todo/TodoForm";
+import TodoList from "../Todo/TodoList";
+import "./Todo.css";
+
+const Todo = ({ todo, todos, addItem, removeItem, lineThrough }) => {
+  return (
+    <div className="Todo-App-Main">
+      <div className="Todo">
+        <TodoForm todo={todo} addItem={addItem} removeItem={removeItem} />
+        <div className="todo-list">
+          <TodoList
+            removeItem={removeItem}
+            todos={todos}
+            lineThrough={lineThrough}
+          />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
